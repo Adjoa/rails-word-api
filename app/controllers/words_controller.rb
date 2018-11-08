0aac8a184d7989aeb114aae3f5842684a76c10b1
Adjoa <adjoa.darien@gmail.com>
2018-11-08 04:15:10 +0000

    words#random accepts and selects from an array of words

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,8 +1,8 @@
 class WordsController < ApplicationController
   
   def random
-    if !params[:word_1].empty? && !params[:word_2].empty?
-      word = [params[:word_1], params[:word_2]].sample
+    if params[:words].length >= 2
+      word = params[:words].sample
       json_response({random_selection: word}, :ok)
     else
       json_response({ message: 'Two words must be submitted to make a selection' }, :not_found)
