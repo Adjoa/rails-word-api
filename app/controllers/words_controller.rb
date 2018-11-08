5c5502258f91b2d1f7b4ce50a4e21f28111faf65
Adjoa <adjoa.darien@gmail.com>
2018-11-08 03:36:49 +0000

    words#random returns a random selection from user entry

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,12 @@
 class WordsController < ApplicationController
   
   def random
-    
+    if !params[:word_1].empty? && !params[:word_2].empty?
+      word = [params[:word_1], params[:word_2]].sample
+      json_response({random_selection: word}, :ok)
+    else
+      json_response({ message: 'Two words must be submitted to make a selection' }, :not_found)
+    end
   end
   
   # def rhyming
