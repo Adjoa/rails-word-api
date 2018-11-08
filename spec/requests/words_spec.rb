638c2089ca88c8cacc8061fb571ead45953bc527
Adjoa <adjoa.darien@gmail.com>
2018-11-08 05:39:54 +0000

    added status OK tests for /words/rhyming and /words/reverse

--- spec/requests/words_spec.rb
+++ spec/requests/words_spec.rb
@@ -10,4 +10,25 @@ RSpec.describe 'Words API', type: :request do
       end
     end
   end
+  
+  describe 'POST /words/rhyming' do
+    context 'when the request is valid' do
+      before { post '/words/rhyming', params: { word: "jelly" } }
+      
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+  
+  describe 'POST /words/reverse' do
+    context 'when the request is valid' do
+      before { post '/words/reverse', params: { word: "jellybean" } }
+      
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+  
 end
